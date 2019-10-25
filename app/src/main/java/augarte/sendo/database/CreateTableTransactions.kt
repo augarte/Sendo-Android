505259508d8e2c03545162c94925af08e6b24036
Aimar Ugarte <ugarteaimar@gmail.com>
2019-10-25 23:51:42 +0200

    updated icons and added foreign keys

--- app/src/main/java/augarte/sendo/database/CreateTableTransactions.kt
+++ app/src/main/java/augarte/sendo/database/CreateTableTransactions.kt
@@ -19,7 +19,8 @@ class CreateTableTransactions {
                 "${DatabaseConstants.TABLE_DAY_WORKOUTID} INTEGER, " +
                 "${DatabaseConstants.TABLE_DAY_CREATEDBY} TEXT NOT NULL, " +
                 "${DatabaseConstants.TABLE_DAY_CREATEDATE} INTEGER NOT NULL, " +
-                "${DatabaseConstants.TABLE_DAY_MODIFYDATE} INTEGER NOT NULL)"
+                "${DatabaseConstants.TABLE_DAY_MODIFYDATE} INTEGER NOT NULL, " +
+                "FOREIGN KEY (${DatabaseConstants.TABLE_DAY_WORKOUTID}) REFERENCES ${DatabaseConstants.TABLE_WORKOUT} (${DatabaseConstants.TABLE_WORKOUT_ID}))"
 
         const val CREATE_TABLE_EXERCISE = "CREATE TABLE IF NOT EXISTS ${DatabaseConstants.TABLE_EXERCISE} " +
                 "(${DatabaseConstants.TABLE_EXERCISE_ID} INTEGER PRIMARY KEY AUTOINCREMENT, " +
@@ -41,14 +42,9 @@ class CreateTableTransactions {
                 "${DatabaseConstants.TABLE_EXERCISEDAY_REPNUM} INTEGER NOT NULL, " +
                 "${DatabaseConstants.TABLE_EXERCISEDAY_CREATEDBY} TEXT NOT NULL, " +
                 "${DatabaseConstants.TABLE_EXERCISEDAY_CREATEDATE} INTEGER NOT NULL, " +
-                "${DatabaseConstants.TABLE_EXERCISEDAY_MODIFYDATE} INTEGER NOT NULL)"
-
-        const val CREATE_TABLE_EXERCISETYPE = "CREATE TABLE IF NOT EXISTS ${DatabaseConstants.TABLE_EXERCISETYPE} " +
-                "(${DatabaseConstants.TABLE_EXERCISETYPE_ID} INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                "${DatabaseConstants.TABLE_EXERCISETYPE_CODE} TEXT NOT NULL, " +
-                "${DatabaseConstants.TABLE_EXERCISETYPE_NAME} TEXT NOT NULL, " +
-                "${DatabaseConstants.TABLE_EXERCISETYPE_CREATEDATE} INTEGER NOT NULL, " +
-                "${DatabaseConstants.TABLE_EXERCISETYPE_MODIFYDATE} INTEGER NOT NULL)"
+                "${DatabaseConstants.TABLE_EXERCISEDAY_MODIFYDATE} INTEGER NOT NULL, " +
+                "FOREIGN KEY (${DatabaseConstants.TABLE_EXERCISEDAY_DAYID}) REFERENCES ${DatabaseConstants.TABLE_DAY} (${DatabaseConstants.TABLE_DAY_ID})," +
+                "FOREIGN KEY (${DatabaseConstants.TABLE_EXERCISEDAY_EXERCISEID}) REFERENCES ${DatabaseConstants.TABLE_EXERCISE} (${DatabaseConstants.TABLE_EXERCISE_ID}))"
 
         const val CREATE_TABLE_SERIE = "CREATE TABLE IF NOT EXISTS ${DatabaseConstants.TABLE_SERIE} " +
                 "(${DatabaseConstants.TABLE_SERIE_ID} INTEGER PRIMARY KEY AUTOINCREMENT, " +
@@ -58,7 +54,8 @@ class CreateTableTransactions {
                 "${DatabaseConstants.TABLE_SERIE_WEEK} INTEGER, " +
                 "${DatabaseConstants.TABLE_SERIE_CREATEDBY} TEXT NOT NULL NOT NULL, " +
                 "${DatabaseConstants.TABLE_SERIE_CREATEDATE} INTEGER NOT NULL, " +
-                "${DatabaseConstants.TABLE_SERIE_MODIFYDATE} INTEGER NOT NULL)"
+                "${DatabaseConstants.TABLE_SERIE_MODIFYDATE} INTEGER NOT NULL," +
+                "FOREIGN KEY (${DatabaseConstants.TABLE_SERIE_EXERCISEDAYID}) REFERENCES ${DatabaseConstants.TABLE_EXERCISEDAY} (${DatabaseConstants.TABLE_EXERCISEDAY_ID}))"
 
         const val CREATE_TABLE_MEASUREMENT = "CREATE TABLE IF NOT EXISTS  ${DatabaseConstants.TABLE_MEASUREMENT} " +
                 "(${DatabaseConstants.TABLE_MEASUREMENT_ID} INTEGER PRIMARY KEY AUTOINCREMENT, " +
@@ -67,7 +64,15 @@ class CreateTableTransactions {
                 "${DatabaseConstants.TABLE_MEASUREMENT_DATE} TEXT NOT NULL," +
                 "${DatabaseConstants.TABLE_MEASUREMENT_CREATEDBY} TEXT NOT NULL, " +
                 "${DatabaseConstants.TABLE_MEASUREMENT_CREATEDATE} INTEGER NOT NULL, " +
-                "${DatabaseConstants.TABLE_MEASUREMENT_MODIFYDATE} INTEGER NOT NULL)"
+                "${DatabaseConstants.TABLE_MEASUREMENT_MODIFYDATE} INTEGER NOT NULL," +
+                "FOREIGN KEY (${DatabaseConstants.TABLE_MEASUREMENT_TYPE}) REFERENCES ${DatabaseConstants.TABLE_MEASURETYPE} (${DatabaseConstants.TABLE_MEASURETYPE_ID}))"
+
+        const val CREATE_TABLE_EXERCISETYPE = "CREATE TABLE IF NOT EXISTS ${DatabaseConstants.TABLE_EXERCISETYPE} " +
+                "(${DatabaseConstants.TABLE_EXERCISETYPE_ID} INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "${DatabaseConstants.TABLE_EXERCISETYPE_CODE} TEXT NOT NULL, " +
+                "${DatabaseConstants.TABLE_EXERCISETYPE_NAME} TEXT NOT NULL, " +
+                "${DatabaseConstants.TABLE_EXERCISETYPE_CREATEDATE} INTEGER NOT NULL, " +
+                "${DatabaseConstants.TABLE_EXERCISETYPE_MODIFYDATE} INTEGER NOT NULL)"
 
         const val CREATE_TABLE_MEASURETYPE = "CREATE TABLE IF NOT EXISTS  ${DatabaseConstants.TABLE_MEASURETYPE} " +
                 "(${DatabaseConstants.TABLE_MEASURETYPE_ID} INTEGER PRIMARY KEY AUTOINCREMENT, " +
--- app/src/main/java/augarte/sendo/database/DatabaseConstants.kt
+++ app/src/main/java/augarte/sendo/database/DatabaseConstants.kt
@@ -4,7 +4,7 @@ class DatabaseConstants {
 
     companion object{
         const val DB_NAME = "Sendo"
-        const val DB_VERSION = 3
+        const val DB_VERSION = 4
 
         const val TABLE_EXERCISE = "exercise"
         const val TABLE_DAY = "day"
